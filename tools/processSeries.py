dce971d7cbe9fbcc3acc9bd01f96f82af30a3ff5
Rohan Sethi <rsethi1@luc.edu>
2023-07-21 01:01:08 -0500

    updated processSeries to standardize

--- tools/processSeries.py
+++ tools/processSeries.py
@@ -5,15 +5,22 @@ import re
 import sys
 import time
 from itertools import repeat
+import argparse
 
 import numpy as np
 import pytraj as pt
 
 ## specific to the protein
 
-protLen = 202
-mask = ":MET@BB"  ## i can't get resid 1 to work correctly, so this is a workaround
-nStruct = 100
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="file with trajectory information", required=True)
+parser.add_argument("-o", "--outpath", help="output folder path", required=True)
+parser.add_argument("-l", "--length", help="length of protein (in number of atoms)", required=True)
+parser.add_argument("-n", "--number", help="number of structures", required=True)
+parser.add_argument("-w", "--workers", help="workers for multithreading", required=False, default=4)
+parser.add_argument("-i", "--ion", help="mask of ions for salt mask", required=False, default=None)
+parser.add_argument("-t", "--hydration", help="mask of hydration for water mask", required=False, default=None)
+
 
 d = {
     "CYS": "C",
@@ -42,27 +49,21 @@ d = {
 def LoadTraj(caseToProcess):
     ## load
 
-    if "pdb" not in caseToProcess:
-        trajName = caseToProcess + ".xtc"
-        protName = caseToProcess + ".pdb"
-        traj = pt.iterload(trajName, protName)
-    else:
-        traj = pt.iterload(caseToProcess)
+    traj = pt.iterload(caseToProcess)
 
     ## get structure info
 
+    mask = ":MET@BB"
     fr_i = traj[0, mask]
     l = pt.get_coordinates(fr_i)
     nStruct = np.shape(l)[1]
-    print("Finding %d structures" % nStruct)
 
     return nStruct, traj
 
 
-def GetTrajData(traj, nStruct):
+def GetTrajData(traj, nStruct, protLen, ionMask, waterMask):
 
-    copies = []
-    timeseriesCopies = []
+    timeseriesContainer = dict()
 
     start = ((nStruct-1) * protLen) + 1
     fin = start + protLen - 1
@@ -80,93 +81,49 @@ def GetTrajData(traj, nStruct):
 
     ## compute number of sodium ions in solvation layers
 
-    sodiumshell = pt.watershell(traj, solute_mask=mask, solvent_mask=":ION")
+    if ionMask != None:
+        sodiumshell = pt.watershell(traj, solute_mask=mask, solvent_mask=ionMask)
+        timeseriesContainer["Salt"] = sodiumshell.values.tolist()
 
     ## compute number of watermolecules in solvation layers
+    
+    if waterMask != None:
+        watershell = pt.watershell(traj, solute_mask=mask, solvent_mask=waterMask)
+        timeseriesContainer["Hydration"] = watershell.values.tolist()
 
-    watershell = pt.watershell(traj, solute_mask=mask, solvent_mask=":W")
-
-    timeseriesContainer = dict()
     timeseriesContainer["RgSeries"] = data.tolist()
     timeseriesContainer["RMSF"] = rmsf.tolist()
-    timeseriesContainer["Salt"] = sodiumshell.values.tolist()
-    timeseriesContainer["Hydration"] = watershell.values.tolist()
     timeseriesContainer["COM"] = com.tolist()
 
     return timeseriesContainer
 
 
-def doit(nStruct, mode=None, case=None):
-    if "trajs3" in case:
-        caseToProcess = os.path.join(case, "system_reduced_all.pdb")
-        dataSeries = "./outdata/traj3Metrics.json"
-    elif "trajs7" in case:
-        caseToProcess = os.path.join(case, "system_reduced_all_MDtraj.pdb")
-        dataSeries = "./data/traj7Metrics.json"
-    else:
-        raise RuntimeError("dunno this case")
+def doit(nStruct, protLen, inputFile, outpath, ionMask, waterMask, workers):
+    
+    dataSeries = os.path.join(outpath, "trajMetrics.json")
 
-    ## inputs
+    print("Generating data from trajs")
+    nStructPossible, traj = LoadTraj(caseToProcess)
+    nStruct = np.min([nStruct, nStructPossible])
+    print(f"Processing {nStruct} out of {nStructPossible}")
 
-    if mode is "generation":
-        print("Generating data from trajs")
-        nStructPossible, traj = LoadTraj(caseToProcess)
-        nStruct = np.min([nStruct, nStructPossible])
-        print("Processing %d" % nStruct)
+    with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
+        indices = [i for i in range(1, nStruct + 1)]
+        results = executor.map(GetTrajData, repeat(traj), indices)
 
-        ## saving series data
+    seriesData = [result for result in results]
 
-        with concurrent.futures.ProcessPoolExecutor(max_workers=25) as executor:
-            indices = [i for i in range(1, nStruct + 1)]
-            results = executor.map(GetTrajData, repeat(traj), indices)
-
-        seriesData = [result for result in results]
-
-        with open(dataSeries, "w") as file:
-            json.dump(seriesData, file)
-
-    else:
-        raise RuntimeError("mode not understood")
+    with open(dataSeries, "w") as file:
+        json.dump(seriesData, file)
 
 
-def helpmsg():
-    scriptName = sys.argv[0]
-    msg = """
-Purpose: 
- 
-Usage:
-"""
-    msg += "  %s -validation" % (scriptName)
-    msg += """
-
-Notes:
-
-"""
-    return msg
-
-
-## main code running script when run from commandline
-
-mode = "generation"
 if __name__ == "__main__":
-    msg = helpmsg()
-    remap = "none"
 
-    if len(sys.argv) < 2:
-        raise RuntimeError(msg)
+    args = parser.parse_args()        
 
-    # Loops over each argument in the command line
-    for i, arg in enumerate(sys.argv):
-        # print(arg)
-        # calls 'doit' with the next argument following the argument '-validation'
-        if arg == "-generation":
-            mode = "generation"
-        if arg == "-postprocess":
-            mode = "postprocess"
-        if arg == "-nstruct":
-            nStruct = int(sys.argv[i + 1])
-        if arg == "-case":
-            arg1 = sys.argv[i + 1]
-            doit(nStruct, mode=mode, case=arg1)
+    start = time.perf_counter()
+    doit(args.number, args.length, args.file, args.outpath, args.ion, args.hydration, args.workers)
+    end = time.perf_counter()
 
-    raise RuntimeError("Arguments not understood")
+    print(f"Time to run = {end-start}")
+
